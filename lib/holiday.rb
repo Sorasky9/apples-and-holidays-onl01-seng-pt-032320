37121b297fc9c351b7ca3df0df9e62c079ba7450
Malcom Fetus <sora.vista@gmail.com>
2020-04-06 02:04:34 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -79,5 +79,7 @@ def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
 holiday_hash.each do |season, holidays|
-  holidays.each do |name, supplies| 
+  holidays.each do |name, supplies|
+  end
+end 
 end
