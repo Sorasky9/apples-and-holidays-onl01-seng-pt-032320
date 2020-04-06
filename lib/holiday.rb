ef6ea30ca00926ee3db5c6e54c28f90b2231cffa
Malcom Fetus <sora.vista@gmail.com>
2020-04-06 02:01:54 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -78,7 +78,8 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
+holiday_hash.each do |season, holiday|
   name = :fourth_of_july && :memorial_day
   if name == :fourth_of_july && :memorial_day
-   puts "#{name}"  
+   puts "#{name}"
 end
