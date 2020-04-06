e5bc39eedee5917d33e991490d4593cedfde104c
Malcom Fetus <sora.vista@gmail.com>
2020-04-06 02:07:44 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -80,7 +80,7 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
 holiday_hash.each do |season, holidays|
   holidays.each do |name, supplies|
-if holidays
+binding.pry
 
   end
 end
